95b0510c1c3afa9d8b157ce8f7b6e2c53445a6c4
Bienvenu Kitutu <bienvenu.kitutu@gmail.com>
2019-09-01 17:52:35 +0100

    add base.html

--- btinda/btinda/settings.py
+++ btinda/btinda/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'btinda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'btinda/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
