4a8a4beaf7ac56cd486db159a84cc6152cfdba30
Bienvenu Kitutu <bienvenu.kitutu@gmail.com>
2019-09-01 22:38:45 +0100

    creation des models course et livreur

--- btinda/courses/models.py
+++ btinda/courses/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Course(models.Model):
+    exp_nom = models.CharField(max_length=100)
+    exp_tel = models.CharField(max_length=10)
+    dest_nom = models.TextField(max_length=100)
+    dest_tel = models.CharField(max_length=10)
+    dest_adresse = models.TextField(max_length=100)
+    dest_colis = models.TextField(max_length=250)
+    date = models.DateTimeField(default=timezone.now, verbose_name="Date enregistrement")
+    livreur = models.ForeignKey('Livreur',null=True, on_delete=models.SET_NULL)
+    
+    class Meta:
+        verbose_name = "course"
+        ordering = ['date']
+    
+    def __str__(self):
+        """ 
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que 
+        nous traiterons plus tard dans l'administration
+        """
+        return self.exp_nom
+
+
+class Livreur(models.Model):
+    nom = models.CharField(max_length=100)
+
+    
+    class Meta:
+        verbose_name = "livreur"
+
+    
+    def __str__(self):
+        """ 
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que 
+        nous traiterons plus tard dans l'administration
+        """
+        return self.nom
